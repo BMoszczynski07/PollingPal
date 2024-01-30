dca25cdc33f91fac98e67b76a65887237a2f2371
Bartosz Moszczyński <109615293+BMoszczynski07@users.noreply.github.com>
2024-01-30 12:49:45 +0100

    initial commit

---
+++ src/main/java/com/example/pollingpalapi/PollingPalApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pollingpalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingPalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollingPalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pollingpalapi/PollingPalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pollingpalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingPalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
