745b02f6486280ca4b8638984a00f5406e7fdfb9
BMoszczynski07 <bmoszczynski2007@gmail.com>
2024-02-17 17:36:42 +0100

    test

--- src/main/java/com/example/pollingpalapi/API/routes/Polls/Polls.java
+++ src/main/java/com/example/pollingpalapi/API/routes/Polls/Polls.java
@@ -21,6 +21,7 @@ public class Polls {
 
     @PostMapping("/like-poll")
     public Response<Object> likePoll(@RequestBody int pollId) {
+        System.out.println("test");
         return new Response<Object>(200, "success");
     }
 
