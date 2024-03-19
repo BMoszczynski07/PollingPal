b8cfaa1a0547391600590b0bf1d6bd36bc1d7ed9
BMoszczynski07 <bmoszczynski2007@gmail.com>
2024-03-19 13:11:16 +0100

    implementing JWT functions

--- src/main/java/com/example/pollingpalapi/API/JWT/SecurityConfig.java
+++ src/main/java/com/example/pollingpalapi/API/JWT/SecurityConfig.java
@@ -1,4 +1,47 @@
 package com.example.pollingpalapi.API.JWT;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/v2/polls/search-polls").permitAll()
+                        .requestMatchers("/api/v2/polls/did-user-vote/{pollId}/{userId}").permitAll()
+                        .requestMatchers("/api/v2/polls/get-polls/{minusDays}").permitAll()
+                        .requestMatchers("/api/v2/polls/get-votes-for-user/{pollId}/{userId}").permitAll()
+                        .requestMatchers("/api/v2/polls/get-votes/{pollId}").permitAll()
+                        .requestMatchers("/api/v2/polls/get-poll-options/{pollId}").permitAll()
+                        .requestMatchers("/api/v2/users/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+
+        return http.build();
+    }
 }
--- src/main/java/com/example/pollingpalapi/API/Models/User/User.java
+++ src/main/java/com/example/pollingpalapi/API/Models/User/User.java
@@ -1,11 +1,17 @@
 package com.example.pollingpalapi.API.Models.User;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
     private int id;
     private String unique_id;
     private String username;
@@ -14,4 +20,39 @@ public class User {
     private String profile_pic;
     private String create_date;
     private boolean verified;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return pass;
+    }
+
+    @Override
+    public String getUsername() {
+        return unique_id;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
