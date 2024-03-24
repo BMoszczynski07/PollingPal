74b021153cad9166ea13e9d39cfdd182aa36bbb4
BMoszczynski07 <bmoszczynski2007@gmail.com>
2024-03-24 18:23:01 +0100

    designing login and register page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent, FormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  formData = {
+    login: '',
+    password: '',
+  };
 
+  handleLogin = (): void => {
+    console.log(this.formData);
+  };
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent, RouterModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-
+  handleRegister = (): void => {};
 }
